721168144b9db2bc1fc75723cb9c9b7f1969f696
Abhishek Kumar <abhishek.kumar529127@gmail.com>
2025-09-14 22:29:11 +0530

    Delete run_mac.sh

--- run_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Checking and installing required libraries..."
-pip install -r requirements.txt
-
-echo "-----------------------------------------"
-echo "Starting the application..."
-echo "-----------------------------------------"
-streamlit run main.py
